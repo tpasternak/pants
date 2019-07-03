tpasternak/pants

commit fa50e6e7a41c135765bdd07af14a1f23a161e758

Switch back to using one threadpool (#7898)
This reverts #7848 and fixes the performance issue found in it.

The first commit is just a revert (with a few manual merge cleanups), the second commit fixes the performance issue.

Fixes #7896

src/rust/engine/fs/Cargo.toml
@@ -12,9 +12,6 @@ bytes = "0.4.5"
 digest = "0.8"
 dirs = "1"
 futures = "^0.1.16"
-futures-cpupool = "0.1"
-# TODO: Switch to a release once https://github.com/alexcrichton/futures-timer/pull/11 and https://github.com/alexcrichton/futures-timer/pull/12 merge
-futures-timer = { git = "https://github.com/pantsbuild/futures-timer", rev = "0b747e565309a58537807ab43c674d8951f9e5a0" }
 glob = "0.2.11"
 grpcio = { git = "https://github.com/pantsbuild/grpc-rs.git", rev = "4dfafe9355dc996d7d0702e7386a6fedcd9734c0", default_features = false, features = ["protobuf-codec", "secure"] }
 hashing = { path = "../hashing" }
@@ -24,7 +21,6 @@ itertools = "0.7.2"
 lazy_static = "1"
 lmdb = { git = "https://github.com/pantsbuild/lmdb-rs.git", rev = "06bdfbfc6348f6804127176e561843f214fc17f8" }
 log = "0.4"
-logging = { path = "../logging" }
 parking_lot = "0.6"
 protobuf = { version = "2.0.6", features = ["with-bytes"] }
 serverset = { path = "../serverset" }
@@ -32,6 +28,10 @@ sha2 = "0.8"
 serde = "1.0"
 serde_derive = "1.0"
 tempfile = "3"
+tokio-codec = "0.1"
+tokio-fs = "0.1.6"
+tokio-io = "0.1.12"
+tokio-threadpool = "0.1.12"
 uuid = { version = "0.7.1", features = ["v4"] }
 
 [dev-dependencies]

src/rust/engine/fs/fs_util/Cargo.toml
@@ -12,8 +12,6 @@ clap = "2"
 env_logger = "0.5.4"
 fs = { path = ".." }
 futures = "^0.1.16"
-# TODO: Switch to a release once https://github.com/alexcrichton/futures-timer/pull/11 and https://github.com/alexcrichton/futures-timer/pull/12 merge
-futures-timer = { git = "https://github.com/pantsbuild/futures-timer", rev = "0b747e565309a58537807ab43c674d8951f9e5a0" }
 hashing = { path = "../../hashing" }
 parking_lot = "0.6"
 protobuf = { version = "2.0.6", features = ["with-bytes"] }

src/rust/engine/fs/fs_util/src/main.rs
@@ -30,7 +30,6 @@ use clap;
 use env_logger;
 use fs;
 use futures;
-use futures_timer;
 
 use rand;
 
@@ -39,7 +38,7 @@ use serde_json;
 use boxfuture::{try_future, BoxFuture, Boxable};
 use bytes::Bytes;
 use clap::{value_t, App, Arg, SubCommand};
-use fs::{GlobMatching, ResettablePool, Snapshot, Store, StoreFileByDigest, UploadSummary};
+use fs::{GlobMatching, Snapshot, Store, StoreFileByDigest, UploadSummary};
 use futures::future::Future;
 use hashing::{Digest, Fingerprint};
 use parking_lot::Mutex;
@@ -257,7 +256,6 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
     .value_of("local-store-path")
     .map(PathBuf::from)
     .unwrap_or_else(Store::default_path);
-  let pool = Arc::new(ResettablePool::new("fsutil-pool-".to_string()));
   let mut runtime = tokio::runtime::Runtime::new().unwrap();
   let (store, store_has_remote) = {
     let (store_result, store_has_remote) = match top_match.values_of("server-address") {
@@ -290,7 +288,6 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
         (
           Store::with_remote(
             &store_dir,
-            pool.clone(),
             &cas_addresses,
             top_match
               .value_of("remote-instance-name")
@@ -315,12 +312,11 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
               std::time::Duration::from_secs(20),
             )?,
             value_t!(top_match.value_of("rpc-attempts"), usize).expect("Bad rpc-attempts flag"),
-            futures_timer::TimerHandle::default(),
           ),
           true,
         )
       }
-      None => (Store::local_only(&store_dir, pool.clone()), false),
+      None => (Store::local_only(&store_dir), false),
     };
     let store = store_result.map_err(|e| {
       format!(
@@ -363,10 +359,9 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
               .map_err(|e| format!("Error canonicalizing path {:?}: {:?}", path, e))?
               .parent()
               .ok_or_else(|| format!("File being saved must have parent but {:?} did not", path))?,
-            pool,
           );
-          let file = posix_fs
-            .stat(PathBuf::from(path.file_name().unwrap()))
+          let file = runtime
+            .block_on(posix_fs.stat(PathBuf::from(path.file_name().unwrap())))
             .unwrap();
           match file {
             fs::Stat::File(f) => {
@@ -420,7 +415,7 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
           })
       }
       ("save", Some(args)) => {
-        let posix_fs = Arc::new(make_posix_fs(args.value_of("root").unwrap(), pool));
+        let posix_fs = Arc::new(make_posix_fs(args.value_of("root").unwrap()));
         let store_copy = store.clone();
         let digest = runtime.block_on(
           posix_fs
@@ -609,8 +604,8 @@ fn expand_files_helper(
     .to_boxed()
 }
 
-fn make_posix_fs<P: AsRef<Path>>(root: P, pool: Arc<ResettablePool>) -> fs::PosixFS {
-  fs::PosixFS::new(&root, pool, &[]).unwrap()
+fn make_posix_fs<P: AsRef<Path>>(root: P) -> fs::PosixFS {
+  fs::PosixFS::new(&root, &[]).unwrap()
 }
 
 fn ensure_uploaded_to_remote(

commit 6d71ba43e671040d80f4208b718e97139c659458

Fix performance regression introduced by #7898
As per
https://docs.rs/tokio-threadpool/0.1.15/tokio_threadpool/fn.blocking.html
when you use a blocking block in a task, it blocks the _entire_ task,
not just the bit which you're marking blocking. This means that when
we're in one giant join_all calling read_dir on a bunch of directories,
we end up serializing them to only read one directory at a time.

This change introduces a new logging::Executor (a slightly weird place
for it, but it depends on the logging stuf, so...) which mirrors the
tokio::Runtime and the futures::sync::oneshot::spawn APIs to
conveniently allow blocking Futures to be spawned in their own tasks, so
that they don't hinder parallelism within a task.

src/rust/engine/fs/Cargo.toml
@@ -13,6 +13,7 @@ glob = "0.2.11"
 ignore = "0.4.4"
 lazy_static = "1"
 log = "0.4"
+logging = { path = "../logging" }
 parking_lot = "0.6"
 tempfile = "3"
 tokio-fs = "0.1.6"

src/rust/engine/fs/fs_util/Cargo.toml
@@ -13,6 +13,7 @@ env_logger = "0.5.4"
 fs = { path = ".." }
 futures = "^0.1.16"
 hashing = { path = "../../hashing" }
+logging = { path = "../../logging" }
 parking_lot = "0.6"
 protobuf = { version = "2.0.6", features = ["with-bytes"] }
 rand = "0.6"

src/rust/engine/fs/fs_util/src/main.rs
@@ -257,7 +257,7 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
     .value_of("local-store-path")
     .map(PathBuf::from)
     .unwrap_or_else(Store::default_path);
-  let mut runtime = tokio::runtime::Runtime::new().unwrap();
+  let runtime = logging::Executor::new();
   let (store, store_has_remote) = {
     let (store_result, store_has_remote) = match top_match.values_of("server-address") {
       Some(cas_address) => {
@@ -360,6 +360,7 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
               .map_err(|e| format!("Error canonicalizing path {:?}: {:?}", path, e))?
               .parent()
               .ok_or_else(|| format!("File being saved must have parent but {:?} did not", path))?,
+            runtime.clone(),
           );
           let file = runtime
             .block_on(posix_fs.stat(PathBuf::from(path.file_name().unwrap())))
@@ -416,7 +417,10 @@ fn execute(top_match: &clap::ArgMatches<'_>) -> Result<(), ExitError> {
           })
       }
       ("save", Some(args)) => {
-        let posix_fs = Arc::new(make_posix_fs(args.value_of("root").unwrap()));
+        let posix_fs = Arc::new(make_posix_fs(
+          args.value_of("root").unwrap(),
+          runtime.clone(),
+        ));
         let store_copy = store.clone();
         let digest = runtime.block_on(
           posix_fs
@@ -605,8 +609,8 @@ fn expand_files_helper(
     .to_boxed()
 }
 
-fn make_posix_fs<P: AsRef<Path>>(root: P) -> fs::PosixFS {
-  fs::PosixFS::new(&root, &[]).unwrap()
+fn make_posix_fs<P: AsRef<Path>>(root: P, executor: logging::Executor) -> fs::PosixFS {
+  fs::PosixFS::new(&root, &[], executor).unwrap()
 }
 
 fn ensure_uploaded_to_remote(

src/rust/engine/fs/src/lib.rs
@@ -545,12 +545,17 @@ pub struct GlobWithSource {
 ///
 #[derive(Clone)]
 pub struct PosixFS {
+  executor: logging::Executor,
   root: Dir,
   ignore: Arc<GitignoreStyleExcludes>,
 }
 
 impl PosixFS {
-  pub fn new<P: AsRef<Path>>(root: P, ignore_patterns: &[String]) -> Result<PosixFS, String> {
+  pub fn new<P: AsRef<Path>>(
+    root: P,
+    ignore_patterns: &[String],
+    executor: logging::Executor,
+  ) -> Result<PosixFS, String> {
     let root: &Path = root.as_ref();
     let canonical_root = root
       .canonicalize()
@@ -575,6 +580,7 @@ impl PosixFS {
       )
     })?;
     Ok(PosixFS {
+      executor,
       root: canonical_root,
       ignore: ignore,
     })
@@ -701,7 +707,11 @@ impl VFS<io::Error> for Arc<PosixFS> {
   }
 
   fn scandir(&self, dir: Dir) -> BoxFuture<Arc<DirectoryListing>, io::Error> {
-    PosixFS::scandir(self, dir).map(Arc::new).to_boxed()
+    self
+      .executor
+      .spawn_in_new_task(PosixFS::scandir(self, dir))
+      .map(Arc::new)
+      .to_boxed()
   }
 
   fn is_ignored(&self, stat: &Stat) -> bool {
@@ -1168,7 +1178,7 @@ mod posixfs_test {
   }
 
   fn new_posixfs<P: AsRef<Path>>(dir: P) -> PosixFS {
-    PosixFS::new(dir.as_ref(), &[]).unwrap()
+    PosixFS::new(dir.as_ref(), &[], logging::Executor::new()).unwrap()
   }
 
   ///

src/rust/engine/fs/store/Cargo.toml
@@ -18,6 +18,7 @@ indexmap = "1.0.2"
 itertools = "0.7.2"
 lmdb = { git = "https://github.com/pantsbuild/lmdb-rs.git", rev = "06bdfbfc6348f6804127176e561843f214fc17f8" }
 log = "0.4"
+logging = { path = "../../logging" }
 parking_lot = "0.6"
 protobuf = { version = "2.0.6", features = ["with-bytes"] }
 serverset = { path = "../../serverset" }

src/rust/engine/fs/store/src/snapshot.rs
@@ -10,6 +10,7 @@ use futures::Future;
 use hashing::{Digest, EMPTY_DIGEST};
 use indexmap::{self, IndexMap};
 use itertools::Itertools;
+use logging;
 use protobuf;
 use std::ffi::OsString;
 use std::fmt;
@@ -467,6 +468,7 @@ impl Snapshot {
   ///
   pub fn capture_snapshot_from_arbitrary_root<P: AsRef<Path> + Send + 'static>(
     store: Store,
+    executor: logging::Executor,
     root_path: P,
     path_globs: PathGlobs,
     digest_hint: Option<Digest>,
@@ -477,7 +479,7 @@ impl Snapshot {
     future::result(digest_hint.ok_or_else(|| "No digest hint provided.".to_string()))
       .and_then(move |digest| Snapshot::from_digest(store, digest))
       .or_else(|_| {
-        let posix_fs = Arc::new(try_future!(PosixFS::new(root_path, &[])));
+        let posix_fs = Arc::new(try_future!(PosixFS::new(root_path, &[], executor)));
 
         posix_fs
           .expand(path_globs)
@@ -605,9 +607,9 @@ mod tests {
     )
     .unwrap();
     let dir = tempfile::Builder::new().prefix("root").tempdir().unwrap();
-    let posix_fs = Arc::new(PosixFS::new(dir.path(), &[]).unwrap());
-    let file_saver = OneOffStoreFileByDigest::new(store.clone(), posix_fs.clone());
     let runtime = tokio::runtime::Runtime::new().unwrap();
+    let posix_fs = Arc::new(PosixFS::new(dir.path(), &[], logging::Executor::new()).unwrap());
+    let file_saver = OneOffStoreFileByDigest::new(store.clone(), posix_fs.clone());
     (store, dir, posix_fs, file_saver, runtime)
   }
 

src/rust/engine/logging/Cargo.toml
@@ -12,6 +12,7 @@ log = "0.4"
 num_enum = "0.1.1"
 parking_lot = "0.6"
 simplelog = "0.5.3"
+tokio = "0.1"
 ui = { path = "../ui" }
 
 [build-dependencies]

src/rust/engine/logging/src/executor.rs
@@ -1,0 +1,88 @@
+use futures::Future;
+use std::sync::Arc;
+use tokio::runtime::Runtime;
+
+#[derive(Clone)]
+pub struct Executor {
+  runtime: Arc<Runtime>,
+}
+
+impl Executor {
+  pub fn new() -> Executor {
+    Executor {
+      runtime: Arc::new(
+        Runtime::new().unwrap_or_else(|e| panic!("Could not initialize Runtime: {:?}", e)),
+      ),
+    }
+  }
+
+  ///
+  /// Start running a Future on a tokio Runtime.
+  ///
+  pub fn spawn<F: Future<Item = (), Error = ()> + Send + 'static>(&self, future: F) {
+    // Make sure to copy our (thread-local) logging destination into the task.
+    // When a daemon thread kicks off a future, it should log like a daemon thread (and similarly
+    // for a user-facing thread).
+    let logging_destination = crate::get_destination();
+    self.runtime.executor().spawn(futures::lazy(move || {
+      crate::set_destination(logging_destination);
+      future
+    }))
+  }
+
+  ///
+  /// Run a Future on a tokio Runtime as a new Task. This is useful for blocking tokio tasks,
+  /// because blocking blocks the entire task called blocking, not just the part of it which is
+  /// marked as blocking. This allows finer granularity of marking tasks as blocking.
+  ///
+  /// See https://docs.rs/tokio-threadpool/0.1.15/tokio_threadpool/fn.blocking.html
+  ///
+  pub fn spawn_in_new_task<
+    Item: Send + 'static,
+    Error: Send + 'static,
+    F: Future<Item = Item, Error = Error> + Send + 'static,
+  >(
+    &self,
+    future: F,
+  ) -> impl Future<Item = Item, Error = Error> {
+    // Make sure to copy our (thread-local) logging destination into the task.
+    // When a daemon thread kicks off a future, it should log like a daemon thread (and similarly
+    // for a user-facing thread).
+    let logging_destination = crate::get_destination();
+    futures::sync::oneshot::spawn(
+      futures::lazy(move || {
+        crate::set_destination(logging_destination);
+        future
+      }),
+      &self.runtime.executor(),
+    )
+  }
+
+  ///
+  /// Run a Future and return its resolved Result.
+  ///
+  /// This should never be called from in a Future context.
+  ///
+  /// This method makes a new Runtime every time it runs, to ensure that the caller doesn't
+  /// accidentally deadlock by using this when a Future attempts to itself call Executor::spawn or
+  /// Executor::spawn_in_new_task. Because it should be used only in very limited situations, this
+  /// overhead is viewed to be acceptable.
+  ///
+  pub fn block_on<
+    Item: Send + 'static,
+    Error: Send + 'static,
+    F: Future<Item = Item, Error = Error> + Send + 'static,
+  >(
+    &self,
+    future: F,
+  ) -> Result<Item, Error> {
+    // Make sure to copy our (thread-local) logging destination into the task.
+    // When a daemon thread kicks off a future, it should log like a daemon thread (and similarly
+    // for a user-facing thread).
+    let logging_destination = crate::get_destination();
+    Runtime::new().unwrap().block_on(futures::lazy(move || {
+      crate::set_destination(logging_destination);
+      future
+    }))
+  }
+}

src/rust/engine/logging/src/lib.rs
@@ -50,6 +50,10 @@ pub mod logger;
 
 pub use logger::{get_destination, set_destination, Destination};
 
+mod executor;
+
+pub use executor::Executor;
+
 pub type Logger = logger::Logger;
 
 use num_enum::TryFromPrimitive;

src/rust/engine/process_execution/Cargo.toml
@@ -17,6 +17,7 @@ futures = "^0.1.16"
 grpcio = { git = "https://github.com/pantsbuild/grpc-rs.git", rev = "4dfafe9355dc996d7d0702e7386a6fedcd9734c0", default_features = false, features = ["protobuf-codec", "secure"] }
 hashing = { path = "../hashing" }
 log = "0.4"
+logging = { path = "../logging" }
 protobuf = { version = "2.0.6", features = ["with-bytes"] }
 sha2 = "0.8"
 store = { path = "../fs/store" }

src/rust/engine/process_execution/src/local.rs
@@ -24,14 +24,21 @@ use bytes::{Bytes, BytesMut};
 
 pub struct CommandRunner {
   store: Store,
+  executor: logging::Executor,
   work_dir: PathBuf,
   cleanup_local_dirs: bool,
 }
 
 impl CommandRunner {
-  pub fn new(store: Store, work_dir: PathBuf, cleanup_local_dirs: bool) -> CommandRunner {
+  pub fn new(
+    store: Store,
+    executor: logging::Executor,
+    work_dir: PathBuf,
+    cleanup_local_dirs: bool,
+  ) -> CommandRunner {
     CommandRunner {
       store,
+      executor,
       work_dir,
       cleanup_local_dirs,
     }
@@ -217,6 +224,7 @@ impl super::CommandRunner for CommandRunner {
     let workdir_path2 = workdir_path.clone();
     let workdir_path3 = workdir_path.clone();
     let store = self.store.clone();
+    let executor = self.executor.clone();
 
     let env = req.env;
     let output_file_paths = req.output_files;
@@ -277,7 +285,7 @@ impl super::CommandRunner for CommandRunner {
           future::ok(store::Snapshot::empty()).to_boxed()
         } else {
           // Use no ignore patterns, because we are looking for explicitly listed paths.
-          future::done(fs::PosixFS::new(workdir_path2, &[]))
+          future::done(fs::PosixFS::new(workdir_path2, &[], executor))
             .map_err(|err| {
               format!(
                 "Error making posix_fs to fetch local process execution output files: {}",
@@ -892,6 +900,7 @@ mod tests {
     let store = Store::local_only(store_dir.path()).unwrap();
     let runner = super::CommandRunner {
       store: store,
+      executor: logging::Executor::new(),
       work_dir: dir,
       cleanup_local_dirs: cleanup,
     };

src/rust/engine/process_executor/Cargo.toml
@@ -9,6 +9,7 @@ publish = false
 clap = "2"
 env_logger = "0.5.4"
 hashing = { path = "../hashing" }
+logging = { path = "../logging" }
 process_execution = { path = "../process_execution" }
 store = { path = "../fs/store" }
 tokio = "0.1"

src/rust/engine/process_executor/src/main.rs
@@ -309,6 +309,7 @@ fn main() {
     }
     None => Box::new(process_execution::local::CommandRunner::new(
       store.clone(),
+      logging::Executor::new(),
       work_dir,
       true,
     )) as Box<dyn process_execution::CommandRunner>,

src/rust/engine/src/context.rs
@@ -7,8 +7,6 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 
-use tokio::runtime::Runtime;
-
 use futures::Future;
 
 use crate::core::{Failure, TypeId};
@@ -21,7 +19,6 @@ use boxfuture::{BoxFuture, Boxable};
 use core::clone::Clone;
 use fs::{safe_create_dir_all_ioerror, PosixFS};
 use graph::{EntryId, Graph, NodeContext};
-use parking_lot::RwLock;
 use process_execution::{self, BoundedCommandRunner};
 use rand::seq::SliceRandom;
 use reqwest;
@@ -42,7 +39,7 @@ pub struct Core {
   pub tasks: Tasks,
   pub rule_graph: RuleGraph<Rule>,
   pub types: Types,
-  runtime: Arc<RwLock<Runtime>>,
+  pub executor: logging::Executor,
   store: Store,
   pub command_runner: BoundedCommandRunner,
   pub http_client: reqwest::r#async::Client,
@@ -78,10 +75,7 @@ impl Core {
     let mut remote_store_servers = remote_store_servers;
     remote_store_servers.shuffle(&mut rand::thread_rng());
 
-    let runtime =
-      Arc::new(RwLock::new(Runtime::new().unwrap_or_else(|e| {
-        panic!("Could not initialize Runtime: {:?}", e)
-      })));
+    let executor = logging::Executor::new();
     // We re-use these certs for both the execution and store service; they're generally tied together.
     let root_ca_certs = if let Some(path) = remote_root_ca_certs_path {
       Some(
@@ -143,6 +137,7 @@ impl Core {
       None => BoundedCommandRunner::new(
         Box::new(process_execution::local::CommandRunner::new(
           store.clone(),
+          executor.clone(),
           work_dir.clone(),
           process_execution_cleanup_local_dirs,
         )),
@@ -158,13 +153,13 @@ impl Core {
       tasks: tasks,
       rule_graph: rule_graph,
       types: types,
-      runtime: runtime,
+      executor: executor.clone(),
       store,
       command_runner,
       http_client,
       // TODO: Errors in initialization should definitely be exposed as python
       // exceptions, rather than as panics.
-      vfs: PosixFS::new(&build_root, &ignore_patterns).unwrap_or_else(|e| {
+      vfs: PosixFS::new(&build_root, &ignore_patterns, executor).unwrap_or_else(|e| {
         panic!("Could not initialize VFS: {:?}", e);
       }),
       build_root: build_root,
@@ -173,51 +168,6 @@ impl Core {
 
   pub fn store(&self) -> Store {
     self.store.clone()
-  }
-
-  ///
-  /// Start running a Future on a tokio Runtime.
-  ///
-  pub fn spawn<F: Future<Item = (), Error = ()> + Send + 'static>(&self, future: F) {
-    // Make sure to copy our (thread-local) logging destination into the task.
-    // When a daemon thread kicks off a future, it should log like a daemon thread (and similarly
-    // for a user-facing thread).
-    let logging_destination = logging::get_destination();
-    self
-      .runtime
-      .read()
-      .executor()
-      .spawn(futures::future::ok(()).and_then(move |()| {
-        logging::set_destination(logging_destination);
-        future
-      }))
-  }
-
-  ///
-  /// Run a Future and return its resolved Result.
-  ///
-  /// This should never be called from in a Future context, or any context where anyone may want to
-  /// spawn something on the runtime using Core::spawn.
-  ///
-  pub fn block_on<
-    Item: Send + 'static,
-    Error: Send + 'static,
-    F: Future<Item = Item, Error = Error> + Send + 'static,
-  >(
-    &self,
-    future: F,
-  ) -> Result<Item, Error> {
-    // Make sure to copy our (thread-local) logging destination into the task.
-    // When a daemon thread kicks off a future, it should log like a daemon thread (and similarly
-    // for a user-facing thread).
-    let logging_destination = logging::get_destination();
-    self
-      .runtime
-      .write()
-      .block_on(futures::future::ok(()).and_then(move |()| {
-        logging::set_destination(logging_destination);
-        future
-      }))
   }
 }
 
@@ -279,6 +229,6 @@ impl NodeContext for Context {
   where
     F: Future<Item = (), Error = ()> + Send + 'static,
   {
-    self.core.runtime.read().executor().spawn(future);
+    self.core.executor.spawn(future);
   }
 }

src/rust/engine/src/lib.rs
@@ -739,7 +739,7 @@ pub extern "C" fn capture_snapshots(
 
   with_scheduler(scheduler_ptr, |scheduler| {
     let core = scheduler.core.clone();
-    core.block_on(
+    core.executor.block_on(
       futures::future::join_all(
         path_globs_and_roots
           .into_iter()
@@ -747,6 +747,7 @@ pub extern "C" fn capture_snapshots(
             let core = core.clone();
             store::Snapshot::capture_snapshot_from_arbitrary_root(
               core.store(),
+              core.executor.clone(),
               root,
               path_globs,
               digest_hint,
@@ -782,6 +783,7 @@ pub extern "C" fn merge_directories(
   with_scheduler(scheduler_ptr, |scheduler| {
     scheduler
       .core
+      .executor
       .block_on(store::Snapshot::merge_directories(
         scheduler.core.store(),
         digests,
@@ -816,7 +818,7 @@ pub extern "C" fn materialize_directories(
   };
 
   with_scheduler(scheduler_ptr, |scheduler| {
-    scheduler.core.block_on(
+    scheduler.core.executor.block_on(
       futures::future::join_all(
         dir_and_digests
           .into_iter()

src/rust/engine/src/scheduler.rs
@@ -266,7 +266,7 @@ impl Scheduler {
 
     // If the join failed (due to `Invalidated`, since that is the only error we propagate), retry
     // the entire set of roots.
-    core.spawn(roots_res.then(move |res| {
+    core.executor.spawn(roots_res.then(move |res| {
       if let Ok(res) = res {
         sender.send(res).map_err(|_| ())
       } else {
@@ -403,6 +403,6 @@ impl NodeContext for RootContext {
   where
     F: Future<Item = (), Error = ()> + Send + 'static,
   {
-    self.core.spawn(future);
+    self.core.executor.spawn(future);
   }
 }